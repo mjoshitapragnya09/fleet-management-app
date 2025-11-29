9534bbf1a0ada77f3cdc7b55d1df0c89bf9cacff
mjoshitapragnya09 <m.joshitap09@gmail.com>
2025-11-29 19:19:55 +0530

    Initial Project Structure2

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
-l
+let emailInput =document.getElementById("email");
+let passwordInput=document.getElementById("password");
+let loginBtn=document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click",loginFunction);
+
+function loginFunction()
+{
+    let email=emailInput.value.trim();
+    let password=passwordInput.value.trim();
+    let validEmail="admin@gmail.com";
+    let validPassword="admin@123";
+    if(email===" validEmail" && password===" validPassword"){
+        alert("Login Successfull");
+        window.location.href="admin.html";
+    }
+    else{
+            alert("Wrong Email or Password")
+    }    
+}
